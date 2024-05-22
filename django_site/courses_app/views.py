569bf0125c755b98e77aa68976d953aaea6235ae
slava <vyacheslavdobr@gmail.com>
2024-05-22 14:27:52 +0300

    [done homework]

--- django_site/courses_app/views.py
+++ django_site/courses_app/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def is_authenticated(request):
-    return render(request, "authentication_information.html")
+    context = {"message": f"This is the Courses page: user - {request.user.username}"}
+    if request.user.is_anonymous:
+        context.update({"message": "You have no permissions to view this page!"})
+        return render(request, "authentication_information.html", context)
+    return render(request, "authentication_information.html", context)
